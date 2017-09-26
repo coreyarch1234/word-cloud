11867ceac7e29f06b12b202a7c1d32e0a30d9a8f
coreyarch1234 <harrilal@mit.edu>
2017-09-26 16:40:05 -0700

    added axios get request

---
+++ components/Name.js
@@ -0,0 +1,35 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var axios = require('axios'); //to make Ajax requests
+
+//Use wolfram alpha api to find rank of name
+class Name extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
+    componentWillMount() {
+        console.log("what is happening");
+        axios.get('https://gbfs.citibikenyc.com/gbfs/en/station_information.json')
+        .then(resp => {
+            console.log("successful api request");
+            console.log(resp.data.data.stations[0]);
+            this.setState({
+                data: [resp.data.data.stations[0]]
+            });
+        })
+        .catch(console.error);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>The data is: {this.state.data}</p>
+            </div>
+        );
+    }
+}
+
+export default Name;
---
+++ js/app.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+import Name from '../components/Name';
+
+
+
+ReactDOM.render(
+    <Name/>,
+    document.getElementById('app')
+);
